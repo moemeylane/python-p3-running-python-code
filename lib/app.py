8f3e3c650aadd09a8226ef209756b03161cac8f9
moemeylane <wangarimaureen668@gmail.com>
2024-08-29 17:07:57 +0300

    complete lab

---
+++ lib/app.py
@@ -0,0 +1,10 @@
+# print("Hello world!")
+# print("Hello sun!")
+# print("Hello sky!")
+# print(f"Hello world ,hello sun, hello sky")
+# print("Hello world!", end="\n ")
+# print("Hello sun!", end=" ")
+# print("Hello sky!", end=" \n")
+# def test_app_py_exists():
+#     assert(path.exists("lib/app.py"))
+print("Hello World! Pass this test, please.")
